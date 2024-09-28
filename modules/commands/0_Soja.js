367aa45ca3872e6d2b5921d1804c5b7f706461a1
Pagal4579 <shreyaahirani402@gmail.com>
2024-09-28 13:28:09 +0530

    Update 0_Soja.js

--- modules/commands/0_Soja.js
+++ modules/commands/0_Soja.js
@@ -18,7 +18,7 @@ module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils
      react.includes("night") ||
 react.includes("Gnn")) {
     var msg = {
-        body: `😂Kumbhkaran😂👈`,attachment: fs.createReadStream(__dirname + `/noprefix/download (1).mp3`)
+        body: `😂Kumbhkaran😂👈`,attachment: fs.createReadStream(__dirname + `/noprefix/gn.mp3`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😈", event.messageID, (err) => {}, true)
