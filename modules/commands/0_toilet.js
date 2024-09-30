b78445b9284bb3caf5ab7d3d6c00f98e9ea24ae6
Pagal4579 <shreyaahirani402@gmail.com>
2024-09-30 07:12:32 +0530

    Update 0_toilet.js

--- modules/commands/0_toilet.js
+++ modules/commands/0_toilet.js
@@ -1,18 +1,18 @@
 module.exports.config = {
-	name: "toilet",
-	version: "1.0.1",
-	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "Toilet 🚽",
-	commandCategory: "Image",
-	usages: "rank",
-	cooldowns: 5,
-	dependencies: {
-	  "fs-extra": "",
-	  "axios": "",
-	  "canvas" :"",
-	  "jimp": ""
-	}
+  name: "toilet",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  description: "Toilet 🚽",
+  commandCategory: "Image",
+  usages: "rank",
+  cooldowns: 5,
+  dependencies: {
+    "fs-extra": "",
+    "axios": "",
+    "canvas" :"",
+    "jimp": ""
+  }
 };
 
 module.exports.onLoad = async() => {
@@ -37,23 +37,23 @@ async function makeImage({ one, two }) {
     let pathImg = __root + `/toilet_${one}_${two}.png`;
     let avatarOne = __root + `/avt_${one}.png`;
     let avatarTwo = __root + `/avt_${two}.png`;
-    
+
     let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
     fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
-    
+
     let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
     fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
-    
+
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
     hon_img.resize(292, 345).composite(circleOne.resize(70, 70), 100, 200).composite(circleTwo.resize(70, 70), 100, 200);
-    
+
     let raw = await hon_img.getBufferAsync("image/png");
-    
+
     fs.writeFileSync(pathImg, raw);
     fs.unlinkSync(avatarOne);
     fs.unlinkSync(avatarTwo);
-    
+
     return pathImg;
 }
 async function circle(image) {
@@ -71,9 +71,9 @@ module.exports.run = async function ({ event, api, args, Currencies }) {
     const mention = Object.keys(event.mentions);
     var one = senderID, two = mention[0];
   await Currencies.increaseMoney(event.senderID, parseInt(hc*rd));
-  
+
   if (!two) return api.sendMessage("Please tag 1 person", threadID, messageID);
   else {
         return makeImage({ one, two }).then(path => api.sendMessage({ body: `...`, attachment: fs.createReadStream(path)}, threadID, () => fs.unlinkSync(path), messageID));
   }
-}
+	    }
